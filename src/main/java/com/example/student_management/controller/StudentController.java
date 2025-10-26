9139af02224c7ee6fcb01bfa7eb4cb73ddd652df
lux1205 <lakshiradha212@gmail.com>
2025-10-26 19:12:41 +0530

    Initial commit: Spring Boot Student Management

---
+++ src/main/java/com/example/student_management/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.student_management.controller;
+
+import com.example.student_management.entity.Student;
+import com.example.student_management.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+}
